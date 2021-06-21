1cf8ddafd473e734b1bd3f15dbf0c2b27e0b97c1
Mehdi Ayache <mehdiayache@hotmail.fr>
2021-06-21 19:15:46 +0200

    :fire: first

---
+++ contacts.js
@@ -0,0 +1,252 @@
+const contacts = [
+  {
+    _id: "60d0c803a72b6d2de16936fc",
+    age: 20,
+    name: "Grant Hardin",
+  },
+  {
+    _id: "60d0c8039d7270a868aa8b06",
+    age: 28,
+    name: "Barron Clements",
+  },
+  {
+    _id: "60d0c803feb9c69781fe316b",
+    age: 31,
+    name: "Cherry Mercado",
+  },
+  {
+    _id: "60d0c8032234329c92ef2746",
+    age: 39,
+    name: "Felicia Duran",
+  },
+  {
+    _id: "60d0c8039241f78d42165832",
+    age: 26,
+    name: "Ayers Larsen",
+  },
+  {
+    _id: "60d0c803e46ccc0d716048c4",
+    age: 23,
+    name: "Louisa Rodriquez",
+  },
+  {
+    _id: "60d0c8035cfa78a93efa0446",
+    age: 32,
+    name: "Lauri Patel",
+  },
+  {
+    _id: "60d0c8037441afa641bd9708",
+    age: 33,
+    name: "Mcdowell Hall",
+  },
+  {
+    _id: "60d0c803e6aedad047cbd090",
+    age: 23,
+    name: "Vaughn Campos",
+  },
+  {
+    _id: "60d0c803ce1a1d8d1a458b27",
+    age: 29,
+    name: "Heidi Henry",
+  },
+  {
+    _id: "60d0c80360889e1baa2ed0dd",
+    age: 40,
+    name: "Gwen Leach",
+  },
+  {
+    _id: "60d0c80389155cb8440c6c52",
+    age: 24,
+    name: "Gail Sosa",
+  },
+  {
+    _id: "60d0c803f2c8abc8f0e03a4f",
+    age: 22,
+    name: "Charles Gibbs",
+  },
+  {
+    _id: "60d0c80351212db0de02d051",
+    age: 29,
+    name: "Frieda Burch",
+  },
+  {
+    _id: "60d0c80324ae2c0bf8456110",
+    age: 37,
+    name: "Melton Garza",
+  },
+  {
+    _id: "60d0c803eba512df0ca05cf3",
+    age: 26,
+    name: "Hawkins Franklin",
+  },
+  {
+    _id: "60d0c80356293cabad3b558d",
+    age: 29,
+    name: "Kinney Hubbard",
+  },
+  {
+    _id: "60d0c8036ddccf8ed1860e48",
+    age: 34,
+    name: "Dana Barnes",
+  },
+  {
+    _id: "60d0c803a24bccf0f51002bd",
+    age: 25,
+    name: "Patton Miller",
+  },
+  {
+    _id: "60d0c803b13bba42219d94c2",
+    age: 35,
+    name: "Erma Tyson",
+  },
+  {
+    _id: "60d0c80306c86011f9363b63",
+    age: 27,
+    name: "Parrish Lee",
+  },
+  {
+    _id: "60d0c803052bd373f5b19118",
+    age: 30,
+    name: "Pugh Vang",
+  },
+  {
+    _id: "60d0c803ce0ebee082bb10f2",
+    age: 27,
+    name: "Albert Whitley",
+  },
+  {
+    _id: "60d0c803874d6ab7cc193b73",
+    age: 35,
+    name: "Imelda Frazier",
+  },
+  {
+    _id: "60d0c803e6e3feda23f713a2",
+    age: 34,
+    name: "Shanna Palmer",
+  },
+  {
+    _id: "60d0c80359ab6ca68b8550f4",
+    age: 32,
+    name: "Fletcher Mueller",
+  },
+  {
+    _id: "60d0c8037e47274f866412ab",
+    age: 35,
+    name: "Barrera Drake",
+  },
+  {
+    _id: "60d0c8038144835e88a8f98e",
+    age: 32,
+    name: "Ray Greene",
+  },
+  {
+    _id: "60d0c80339e905bc0eb1b906",
+    age: 35,
+    name: "Gordon Woods",
+  },
+  {
+    _id: "60d0c8031fe1caa2ff25a344",
+    age: 21,
+    name: "Briana Decker",
+  },
+  {
+    _id: "60d0c803407a9a96e1333d11",
+    age: 31,
+    name: "Concetta Barr",
+  },
+  {
+    _id: "60d0c803b752c46a506ddccb",
+    age: 22,
+    name: "Virgie Monroe",
+  },
+  {
+    _id: "60d0c803cd47a679cfe77b1c",
+    age: 25,
+    name: "Faith Barber",
+  },
+  {
+    _id: "60d0c803e27fb9ea2d712411",
+    age: 39,
+    name: "Melanie Mcfarland",
+  },
+  {
+    _id: "60d0c80305447a6a2c9a7537",
+    age: 35,
+    name: "Alison Bailey",
+  },
+  {
+    _id: "60d0c803a872afb65c96fa19",
+    age: 31,
+    name: "Karina Herman",
+  },
+  {
+    _id: "60d0c803a42c8c5d1a328dff",
+    age: 31,
+    name: "Billie Luna",
+  },
+  {
+    _id: "60d0c80395fd92a5628e5695",
+    age: 39,
+    name: "Bray Ryan",
+  },
+  {
+    _id: "60d0c8038f390baf2746b97f",
+    age: 38,
+    name: "Kathrine Mack",
+  },
+  {
+    _id: "60d0c8037663461950646ba7",
+    age: 22,
+    name: "Bowman Espinoza",
+  },
+  {
+    _id: "60d0c803f6fe221d8168318b",
+    age: 20,
+    name: "Agnes Goodman",
+  },
+  {
+    _id: "60d0c803ae006f53bbb6f77b",
+    age: 26,
+    name: "Essie Hunt",
+  },
+  {
+    _id: "60d0c803cc29c23c47a3acb1",
+    age: 27,
+    name: "Calderon Pruitt",
+  },
+  {
+    _id: "60d0c803a77eb71796004261",
+    age: 25,
+    name: "Olga Gutierrez",
+  },
+  {
+    _id: "60d0c80350c94dfb24cff61b",
+    age: 39,
+    name: "Golden Cantrell",
+  },
+  {
+    _id: "60d0c803dbabae2cfd0212c4",
+    age: 23,
+    name: "Katrina Estrada",
+  },
+  {
+    _id: "60d0c80339e595735034b853",
+    age: 34,
+    name: "Margret May",
+  },
+  {
+    _id: "60d0c803333ae522f94a292a",
+    age: 24,
+    name: "Kasey Cochran",
+  },
+  {
+    _id: "60d0c803ee232a7df05edd28",
+    age: 26,
+    name: "Suzette Santos",
+  },
+  {
+    _id: "60d0c803bfcafc87c671c8c0",
+    age: 26,
+    name: "Austin Shaw",
+  },
+];
